cb767431514e49795b9ab1eeb85b841db8795997
YH <rladudgh337@naver.com>
2020-04-30 20:14:57 +0900

    회원가입+리스트

---
+++ src/main/java/com/web/domain/User.java
@@ -0,0 +1,16 @@
+package com.web.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class User {
+
+    private String userId;
+    private String UserPw;
+    private String userName;
+
+}
