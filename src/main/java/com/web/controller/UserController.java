c399d306c00fa63b77a8c89d7d9e0124bc4b98f5
YH <rladudgh337@naver.com>
2020-05-03 22:59:54 +0900

    Spring Security를 이용하지 않은 로그인/ 로그아웃 기능 구현

--- src/main/java/com/web/controller/UserController.java
+++ src/main/java/com/web/controller/UserController.java
@@ -6,11 +6,10 @@ import com.web.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -55,10 +54,32 @@ public class UserController {
     public void login(){
 
     }
+    @PostMapping("/login")
+    public String postLogin(@RequestParam("userId") String userId, @RequestParam("userPw") String userPw,
+                            HttpSession session, RedirectAttributes rttr){
+        User user=userRepository.findUserByUserId(userId);
+
+        if(user==null){
+            rttr.addFlashAttribute("result","아이디가 존재하지 않습니다.");
+            return "redirect:/users/login_failed";
+        }
+        if(!userPw.equals(user.getUserPw())){
+            rttr.addFlashAttribute("result","비밀번호가 맞지 않습니다.");
+            return "redirect:/users/login_failed";
+        }
+
+        session.setAttribute("user",user);
+        return "redirect:/";
+    }
     @GetMapping("/login_failed")
     public void login_failed(){
-
     }
+    @GetMapping("/logout")
+    public String logout(HttpSession session){
+        session.removeAttribute("user");
+        return "redirect:/users/login";
+    }
+
     @GetMapping("/profile")
     public void profile(){
 
--- src/main/java/com/web/repository/UserRepository.java
+++ src/main/java/com/web/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.web.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-
+    public User findUserByUserId(String userId);
 }
