4e0c89d031d65ca6f53fac7d6f2450a2b4590dc2
Youngho62 <48435036+Youngho62@users.noreply.github.com>
2020-06-09 23:49:55 +0900

    메인 수정

--- src/main/java/com/web/controller/HomeController.java
+++ src/main/java/com/web/controller/HomeController.java
@@ -1,24 +1,11 @@
 package com.web.controller;
 
-import com.web.domain.User;
-import com.web.repository.UsersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class HomeController {
-    @Autowired
-    private UsersRepository usersRepository;
 
-    private List<User> users=new ArrayList<User>();
     @GetMapping("/")
     public String index(){
         return "/index";
--- src/test/java/com/web/BootjpaApplicationTests.java
+++ src/test/java/com/web/BootjpaApplicationTests.java
@@ -1,89 +1,8 @@
 package com.web;
 
-import com.web.domain.Files;
-import com.web.domain.User;
-import com.web.domain.WebBoard;
-import com.web.repository.FilesRepository;
-import com.web.repository.UsersRepository;
-import com.web.repository.WebBoardRepository;
-import com.web.repository.WebReplyRepository;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-import java.util.stream.IntStream;
 
 @SpringBootTest
 class BootjpaApplicationTests {
-    @Autowired
-    private UsersRepository usersRepository;
 
-    @Autowired
-    private WebBoardRepository boardRepository;
-
-    @Autowired
-    private WebReplyRepository webReplyRepository;
-
-    @Autowired
-    private FilesRepository filesRepository;
-
-
-    @Test
-    public void test2(){
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DATE, -1);
-        String str = sdf.format(cal.getTime());
-        str=str.replace("-", File.separator);
-        System.out.println(str);
-    }
-
-
-
-    @Test
-    void boardInsert() {
-        IntStream.range(0,300).forEach(i->{
-            WebBoard board= new WebBoard();
-
-            board.setTitle("Test Title"+i);
-            board.setContent("Test Content..."+i);
-            board.setWriter("rladudgh337");
-            boardRepository.save(board);
-        });
-
-    }
-
-    @Test
-    public void testReply(){
-        int num=usersRepository.findCountByUserId("asddasdasxc");
-        System.out.println("☆★☆★☆★☆★☆★☆★☆★☆★☆★☆★$☆★☆★☆★☆★☆★");
-        System.out.println(num);
-    }
-
-
-    @Test
-    public void makeUser(){
-        IntStream.range(0,100).forEach(i->{
-            User user=new User();
-            if(i<50){
-                user.setUserId("TEST_BASIC"+i);
-
-            }else if(i<90){
-                user.setUserId("TEST_MANAGER"+i);
-            }else{
-                user.setUserId("TEST_ADMIN"+i);
-            }
-            user.setUserPw("1234");
-            user.setUserName("TEST_USER"+i);
-            user.setUserPhone("01011112222");
-            user.setUserEmail("testEmail@google.com");
-            usersRepository.save(user);
-        });
-
-    }
 }
